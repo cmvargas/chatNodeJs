c741adbfb0e25285a3bd0f93b83346f5ad310259
cmvargas <cmvargase@hotmail.com>
2019-09-28 13:58:50 -0500

    Creando vista de chat input username y message

---
+++ index.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const socketIO = require('socket.io');
+
+//settings
+app.set('port', process.env.PORT || 3000);
+
+//files static
+app.use(express.static(path.join(__dirname,'public')));
+
+//start server
+const server = app.listen(app.get('port'),()=>{
+  console.log('Server in port', app.get('port'));
+});
+//websockets
+const io = socketIO(server)
+
+io.on('connection', (socket)=>{
+  console.log('New conecction',socket.id);
+});
